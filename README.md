af16262052063b2e9c34af117dae584bae5a8849
Nandakumar Chandrasekhar <navanitachora@gmail.com>
2020-04-14 16:24:47 -0700

    Modified pandoc_reader by adding capabilities to read YAML header

--- README.md
+++ README.md
@@ -1,23 +1,16 @@
-pandoc_reader
-=============
+# pandoc_reader
 
 A pandoc [markdown] reader plugin for [pelican]
 
+## Requirements
 
-Requirements
-------------
+- [pandoc] in $PATH
 
-  - [pandoc] in $PATH
-
-
-Installation
-------------
+## Installation
 
 Instructions for installation of pelican plugins can be obtained from the [pelican plugin manual](https://github.com/getpelican/pelican-plugins/blob/master/Readme.rst).
 
-
-Configuration
--------------
+## Configuration
 
 Additional command line parameters can be passed to pandoc via the PANDOC_ARGS parameter.
 
@@ -37,15 +30,13 @@ PANDOC_EXTENSIONS parameter.
       '-citations'
     ]
 
-Contributing
-------------
+## Contributing
 
 1. Fork it
 2. Create your feature branch (`git checkout -b my-new-feature`)
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
 5. Create new Pull Request
-
 
 [markdown]: http://daringfireball.net/projects/markdown/
 [pandoc]: http://johnmacfarlane.net/pandoc/
--- pandoc_reader.py
+++ pandoc_reader.py
@@ -1,48 +1,77 @@
+"""Reader that processes Pandoc Markdown and returns HTML 5."""
 import subprocess
+
 from pelican import signals
 from pelican.readers import BaseReader
 from pelican.utils import pelican_open
 
+
 class PandocReader(BaseReader):
+    """Process files written in Pandoc Markdown."""
+
     enabled = True
     file_extensions = ['md', 'markdown', 'mkd', 'mdown']
 
-    def read(self, filename):
-        with pelican_open(filename) as fp:
-            text = list(fp.splitlines())
+    def read(self, source_path):
+        """Parse Pandoc Markdown and return HTNL 5 output and metadata."""
+        content = ""
 
-        metadata = {}
-        for i, line in enumerate(text):
-            kv = line.split(':', 1)
-            if len(kv) == 2:
-                name, value = kv[0].lower(), kv[1].strip()
-                metadata[name] = self.process_metadata(name, value)
-            else:
-                content = "\n".join(text[i:])
-                break
+        with pelican_open(source_path) as file_content:
+            content = file_content
 
-        extra_args = self.settings.get('PANDOC_ARGS', [])
-        extensions = self.settings.get('PANDOC_EXTENSIONS', '')
-        if isinstance(extensions, list):
-            extensions = ''.join(extensions)
+            # Parse metadata
+            metadata = self._process_metadata(list(content.splitlines()))
 
-        pandoc_cmd = ["pandoc", "--from=markdown" + extensions, "--to=html5"]
-        pandoc_cmd.extend(extra_args)
+            # Get arguments and extensions
+            extra_args = self.settings.get('PANDOC_ARGS', [])
+            extensions = self.settings.get('PANDOC_EXTENSIONS', '')
+            if isinstance(extensions, list):
+                extensions = ''.join(extensions)
 
-        proc = subprocess.Popen(pandoc_cmd,
-                                stdin = subprocess.PIPE,
-                                stdout = subprocess.PIPE)
+            # Construct Pandoc command
+            pandoc_cmd = [
+                "pandoc", "-f", "markdown" + extensions, "-t", "html5"
+            ]
+            pandoc_cmd.extend(extra_args)
 
-        output = proc.communicate(content.encode('utf-8'))[0].decode('utf-8')
-        status = proc.wait()
-        if status:
-            raise subprocess.CalledProcessError(status, pandoc_cmd)
+            # Execute and retrieve HTML 5 output
+            proc = subprocess.Popen(
+                pandoc_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE
+            )
+            output = proc.communicate(content.encode('utf-8'))[0].decode('utf-8')
+            status = proc.wait()
+            if status:
+                raise subprocess.CalledProcessError(status, pandoc_cmd)
 
         return output, metadata
 
+    def _process_metadata(self, text):
+        """Process YAML metadata and export."""
+        metadata = {}
+
+        # Find the end of the YAML block
+        lines = text[1:]
+        yaml_end = ""
+        for line_num, line in enumerate(lines):
+            if line in ["---", "..."]:
+                yaml_end = line_num
+                break
+
+        # Process the YAML block
+        for line in lines[:yaml_end]:
+            metalist = line.split(':', 1)
+            if len(metalist) == 2:
+                key, value = metalist[0].lower(), metalist[1].strip()
+                metadata[key] = self.process_metadata(key, value)
+        return metadata
+
+
 def add_reader(readers):
+    """Add the PandocReader as the reader for all Pandoc Markdown files."""
     for ext in PandocReader.file_extensions:
         readers.reader_classes[ext] = PandocReader
 
+
 def register():
+    """Register the PandocReader."""
     signals.readers_init.connect(add_reader)
